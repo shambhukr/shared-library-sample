69c4838dc08912f7a9589a2f82489d036acc8465
shambhukr <33447857+shambhukr@users.noreply.github.com>
2019-08-06 18:57:30 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,5 +1,9 @@
 def info(message){
     echo "Info: ${message}"
+    def props = readJSON file: "${WORKSPACE}/input.json"
+    assert props['fruit'] == 'Apple'
+    assert props.size == 'Large'
+    assert props.color == 'Green' : "Color should be Red"
 }
 
 def warning(message){
